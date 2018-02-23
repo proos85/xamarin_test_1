0083b5d616e549921921f3f8455253822accafd7
Patrick Roos <patrick@point42.nl>
2018-02-23 16:34:07 +0100

    Add project files.

---
+++ XamarinForms1/XamarinForms1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinForms1.Models;
+
+namespace XamarinForms1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
