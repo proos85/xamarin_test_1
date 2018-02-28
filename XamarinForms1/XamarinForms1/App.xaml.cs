d1f0bb08297d97f035dd57a1956b7adaa7069a94
Patrick Roos <patrick@point42.nl>
2018-02-28 23:28:04 +0100

    Added App Center Analytics and Crashes API

--- XamarinForms1/XamarinForms1/App.xaml.cs
+++ XamarinForms1/XamarinForms1/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
-
-using XamarinForms1.Views;
+﻿using XamarinForms1.Views;
 using Xamarin.Forms;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace XamarinForms1
 {
@@ -11,14 +13,13 @@ namespace XamarinForms1
 		public App ()
 		{
 			InitializeComponent();
-
-
             MainPage = new MainPage();
         }
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
+            // Handle when your app starts
+		    AppCenter.Start("ios=86f978f4-e62a-4445-9571-2f2b7617975d;", typeof(Analytics), typeof(Crashes));
 		}
 
 		protected override void OnSleep ()
