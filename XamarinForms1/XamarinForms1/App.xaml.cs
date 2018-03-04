2c343e09a84b891c20096bc52a5e1992fff038ab
Patrick Roos <patrick@point42.nl>
2018-03-04 18:33:10 +0100

    Disable App Center Push Notfication for now

--- XamarinForms1/XamarinForms1/App.xaml.cs
+++ XamarinForms1/XamarinForms1/App.xaml.cs
@@ -3,7 +3,6 @@ using Xamarin.Forms;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
-using Microsoft.AppCenter.Push;
 using XamarinForms1.ViewModels;
 
 namespace XamarinForms1
@@ -26,7 +25,7 @@ namespace XamarinForms1
 		protected override void OnStart ()
 		{
             // Handle when your app starts
-		    AppCenter.Start("ios=86f978f4-e62a-4445-9571-2f2b7617975d;", typeof(Analytics), typeof(Crashes), typeof(Push));
+		    AppCenter.Start("ios=86f978f4-e62a-4445-9571-2f2b7617975d;", typeof(Analytics), typeof(Crashes));
         }
 
 		protected override void OnSleep ()
